41ff1400baded3a87f5f7c2088e4e645683ab10e
Valério Correa Sanches <valeriocs.oi@gmail.com>
2018-03-28 17:13:16 -0300

    Create Condicionais com v-if.vue

---
+++ Condicionais com v-if.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div
+  p(v-if="show") Teste 1
+    p(v-else) Substituto do Teste 1
+  p Teste 2
+  v-btn(@click="show = !show" dark ) Remover o teste 1
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
