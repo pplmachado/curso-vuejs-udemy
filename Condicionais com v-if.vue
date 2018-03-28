a39309b8ea7dfce6ac6ab1fea525ac6a09e3fceb
Valério Correa Sanches <valeriocs.oi@gmail.com>
2018-03-28 17:20:08 -0300

    Update Condicionais com v-if.vue

--- Condicionais com v-if.vue
+++ Condicionais com v-if.vue
@@ -3,6 +3,10 @@ div
   p(v-if="show") Teste 1
     p(v-else) Substituto do Teste 1
   p Teste 2
+  //-  A diferença do v-if para o v-show é que no segundo ele esconde
+  //- o elemento e no v-if ele não chega nem a ser renderizado, como se fosse deletado da aplicação.
+  template(v-show="show")
+    | Dentro do template
   v-btn(@click="show = !show" dark ) Remover o teste 1
 </template>
 
@@ -15,3 +19,6 @@ export default {
   },
 };
 </script>
+
+<style lang="css">
+</style>
