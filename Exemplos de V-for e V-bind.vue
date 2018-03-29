76dd763d686e3e9ecfcfb430909bcc708a172753
Valério Correa Sanches <valeriocs.oi@gmail.com>
2018-03-29 09:20:59 -0300

    Create Exemplos de V-for e V-bind.vue

---
+++ Exemplos de V-for e V-bind.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div
+  ul
+    //-  Usando o v-bind ":key" eu consigo  fazer que o vueJS interprete a 
+    //- entrada de forma diferente, não substituindo a entrada apenas, mas salvando a lista.
+    li(color="success" v-for="(ingrediente, i) in ingredientes" :key='ingrediente')  {{ ingrediente }} ({{ i }})
+    li(v-for="(pessoa, i) in pessoas")
+      div(v-for="(valor, key, index) in pessoa") > {{ key }}:  {{ valor }} / - {{ index }}
+  div
+    v-btn(@click="ingredientes.push('beterraba')") Colocar beterraba
+    ul
+      span(v-for="n in 10") {{ n }}
+</template>
+<script>
+export default {
+  data: () => ({
+    ingredientes: [
+      'carne',
+      'fruta',
+      'bolachas',
+    ],
+    pessoas: [
+      {
+        nome: 'Percival',
+        idade: 56,
+        cor: 'preto',
+      },
+      {
+        nome: 'Rebosteio',
+        idade: 65,
+        cor: 'azul',
+      },
+      {
+        nome: 'Josefa',
+        idade: 22,
+        cor: 'amarelo',
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="css">
+</style>
